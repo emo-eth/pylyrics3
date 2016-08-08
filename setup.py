2d47d66eb56779f2bf35d9304195312a2f5594cd
Sasha Perigo <sasha.perigo@gmail.com>
2016-08-07 21:37:12 -0400

    Updated packages in setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name = "pylyrics3",
     version = "0.1.0",
-    packages = ["pylyrics3"],
+    packages = find_packages(),
 
     install_requires = ['beautifulsoup4>=4.0.0',
                         'requests>=2.0.0'],
