3b25ad07eb2a1d4c38b9b187343c7835cc168a7b
jameswenzel <wenzel.james.r@gmail.com>
2017-09-17 14:18:36 -0700

    version update

--- upload_script.sh
+++ upload_script.sh
@@ -3,7 +3,7 @@
 git add .
 git commit -m "version update"
 git push origin master
-git tag 1.0.2
+git tag 1.0.3
 git push --tags origin master
 python setup.py register -r pypi
 python setup.py sdist upload -r pypi
